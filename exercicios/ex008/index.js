0ee304fcee02c88674c85a3d3ea80d93bf260e94
Leonardo Barbosa <leobarbosajf@hotmail.com>
2023-11-20 20:12:22 -0300

    Criação ex008, 009, 010 e 011

---
+++ exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60){ //CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou o limite de velocidade permitida. MULTADO')
+}
+console.log('Dirija sempre usando sinto de segurança!')
