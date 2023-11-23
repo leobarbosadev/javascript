db658c70df92a3c853146de267047063e5980722
Leonardo Barbosa <leobarbosajf@hotmail.com>
2023-11-23 16:44:39 -0300

    Estruturas de repetição

---
+++ exercicios/ex016/test.js
@@ -0,0 +1,28 @@
+/*console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')*/
+
+/*var contador = 1
+while (contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++ // MESMA COISA QUE contador = contador + 1
+}*/
+
+/*var contador = 1
+do{
+    console.log(`Passo ${contador}`)
+    contador++ // MESMA COISA QUE contador = contador + 1
+}while (contador <= 6)*/
+
+/*for(var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}*/
+
+console.log('Vai começar...')
+for(var c = 1;c <= 4; c++){
+    console.log(c)
+}
+console.log('FIM!')
